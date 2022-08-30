f2fcc0993e7aea145ca63e67e186776c7d6ae81c
Eron Moraes <eron.moraes7@gmail.com>
2022-08-30 16:16:05 -0300

    .

--- curso_irts_interassys_testesweb/ecommerce_prestashop/src/test/java/homepage/HomePageTests.java
+++ curso_irts_interassys_testesweb/ecommerce_prestashop/src/test/java/homepage/HomePageTests.java
@@ -1,5 +1,17 @@
 package homepage;
 
-public class HomePageTests {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.MatcherAssert.is;
+
+import org.junit.jupiter.api.Test;
+
+public class HomePageTests extends BaseTests {
+	
+	@Test
+	public void testContarProdutos_oitoProdutosDiferentes() {
+		carregarPaginaInicial();
+		assertThat(homePage.contarProdutos(), is (8))	
+		
+	}
 
 }
