7c6cfb595f09f0a68469b634f4d6c7dbb3a85fbd
Eron Moraes <eron.moraes7@gmail.com>
2022-10-13 15:50:00 -0300

    tentando

--- curso_irts_interassys_testesweb/ecommerce_prestashop/src/test/java/homepage/HomePageTests.java
+++ curso_irts_interassys_testesweb/ecommerce_prestashop/src/test/java/homepage/HomePageTests.java
@@ -1,8 +1,8 @@
 package homepage;
 
-import static org.hamcrest.Matcher.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
 import base.BaseTests;
@@ -15,7 +15,14 @@ public class HomePageTests extends BaseTests {
 		assertThat(homePage.contarProdutos(), is(8));
 	}
 
-}
+
+
+private Matcher is(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 
 @Test 
 public void testValidarCarrinhoZerado_ZeroItensNoCarrinho() {
@@ -23,3 +30,5 @@ public void testValidarCarrinhoZerado_ZeroItensNoCarrinho() {
 	System.out.println(produtosNoCarrinho);
 	assertThat(produtosNoCarrinho, is(0));
 }
+}
+	
